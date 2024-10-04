ddc2b2142d9b6a9938884c5bb55c56dade989f30
Lenny Thobejane <Lennythobejane@gmail.com>
2024-10-04 09:24:07 +0200

    register form update

--- src/common/layout/containers/SignupModal.js
+++ src/common/layout/containers/SignupModal.js
@@ -8,6 +8,9 @@ import TextField from "@mui/material/TextField";
 import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 
+// Components
+import InputFileUpload from "../../components/UploadButton";
+
 const SignupModal = ({ open, handleClose }) => {
     const { handleSubmit, control } = useForm();
 
@@ -20,54 +23,103 @@ const SignupModal = ({ open, handleClose }) => {
 
             <form onSubmit={handleSubmit(handleSignup)}>
                 <Stack spacing={2} padding={2}>
-                    <Controller
-                        name="email"
-                        rules={{
-                            required: "Email is required",
-                        }}
-                        control={control}
-                        render={({ field, fieldState: { error } }) => (
-                            <TextField
-                                {...field}
-                                error={!!error}
-                                label="Email *"
-                                variant="outlined"
-                                fullWidth
-                            />
-                        )}
-                    />
-                    <Controller
-                        name="password"
-                        rules={{
-                            required: "Password is required",
-                        }}
-                        control={control}
-                        render={({ field, fieldState: { error } }) => (
-                            <TextField
-                                {...field}
-                                type="password"
-                                error={!!error}
-                                label="Password *"
-                                variant="outlined"
-                                fullWidth
-                            />
-                        )}
-                    />
-                    <Controller
-                        name="cell"
-                        control={control}
-                        render={({ field, fieldState: { error } }) => (
-                            <TextField
-                                {...field}
-                                error={!!error}
-                                label="Cell (optional)"
-                                variant="outlined"
-                                fullWidth
-                            />
-                        )}
-                    />
-                    <Button type="submit" variant="contained" color="primary" fullWidth>
-                        Login
+                    <Stack direction={"row"} spacing={2}>
+                        <Controller
+                            name="first_name"
+                            rules={{
+                                required: "First Name is required",
+                            }}
+                            control={control}
+                            render={({ field, fieldState: { error } }) => (
+                                <TextField
+                                    {...field}
+                                    error={!!error}
+                                    label="First Name *"
+                                    variant="outlined"
+                                    fullWidth
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="last_name"
+                            rules={{
+                                required: "Last Name is required",
+                            }}
+                            control={control}
+                            render={({ field, fieldState: { error } }) => (
+                                <TextField
+                                    {...field}
+                                    error={!!error}
+                                    label="Last Name *"
+                                    variant="outlined"
+                                    fullWidth
+                                />
+                            )}
+                        />
+                    </Stack>
+                    <Stack direction={"row"} spacing={2}>
+                        <Controller
+                            name="cell"
+                            control={control}
+                            render={({ field, fieldState: { error } }) => (
+                                <TextField
+                                    {...field}
+                                    error={!!error}
+                                    label="Cell (optional)"
+                                    variant="outlined"
+                                    fullWidth
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="profile_picture"
+                            control={control}
+                            render={({ field, fieldState: { error } }) => (
+                                <InputFileUpload
+                                    title={"Upload Profile Picture (Optional)"}
+                                    field={field}
+                                />
+                            )}
+                        />
+                    </Stack>
+                    <Stack direction={"row"} spacing={2}>
+                        <Controller
+                            name="email"
+                            rules={{
+                                required: "Email is required",
+                            }}
+                            control={control}
+                            render={({ field, fieldState: { error } }) => (
+                                <TextField
+                                    {...field}
+                                    error={!!error}
+                                    label="Email *"
+                                    variant="outlined"
+                                    fullWidth
+                                />
+                            )}
+                        />
+                        <Controller
+                            name="password"
+                            rules={{
+                                required: "Password is required",
+                            }}
+                            control={control}
+                            render={({ field, fieldState: { error } }) => (
+                                <TextField
+                                    {...field}
+                                    type="password"
+                                    error={!!error}
+                                    label="Password *"
+                                    variant="outlined"
+                                    fullWidth
+                                />
+                            )}
+                        />
+                    </Stack>
+
+                    <Button type="submit" variant="contained" color="warning" fullWidth>
+                        Register
                     </Button>
                 </Stack>
             </form>
